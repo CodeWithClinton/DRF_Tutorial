76eaf766bf3bd8025d59ecf53f9c91c868ce5f1c
Clinton Nwachukwu <Clintonmatics@gmail.com>
2023-06-02 09:38:07 +0100

    data validation and updating orders

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from importlib.resources import read_binary
 from itertools import product
+from django.db import transaction
 from rest_framework import serializers
 from  storeapp.models import *
 
@@ -123,6 +124,46 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ['id', "placed_at", "pending_status", "owner", "items"]
         
 
+
+
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+    
+    
+    
+    def validate_cart_id(self, cart_id):
+        if not Cart.objects.filter(pk=cart_id).exists():
+            raise serializers.ValidationError("This cart_id is invalid")
+        
+        elif not Cartitems.objects.filter(cart_id=cart_id).exists():
+            raise serializers.ValidationError("Sorry your cart is empty")
+        
+        return cart_id
+    
+    
+    
+    def save(self, **kwargs):
+        with transaction.atomic():
+            cart_id = self.validated_data["cart_id"]
+            user_id = self.context["user_id"]
+            order = Order.objects.create(owner_id = user_id)
+            cartitems = Cartitems.objects.filter(cart_id=cart_id)
+            orderitems = [
+                OrderItem(order=order, 
+                    product=item.product, 
+                    quantity=item.quantity
+                    )
+            for item in cartitems
+            ]
+            OrderItem.objects.bulk_create(orderitems)
+            # Cart.objects.filter(id=cart_id).delete()
+            return order
+
+
+class UpdateOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order 
+        fields = ["pending_status"]
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
--- api/views.py
+++ api/views.py
@@ -13,7 +13,7 @@ from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 
@@ -78,15 +78,44 @@ class CartItemViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
-    serializer_class = OrderSerializer
     
+    http_method_names = ["get", "patch", "post", "delete", "options", "head"]
+    
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+            
+    
+    
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(data=request.data, context={"user_id": self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+        
+    
+
+    
+    
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateOrderSerializer
+        return OrderSerializer
+       
     
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
             return Order.objects.all()
         return Order.objects.filter(owner=user)
+    
+    # def get_serializer_context(self):
+    #     return {"user_id": self.request.user.id}
             
         
 
