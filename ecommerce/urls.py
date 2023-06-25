43c3f9360fdb11e2b551c8e360f6d01b6ecc1417
Clinton Nwachukwu <Clintonmatics@gmail.com>
2023-06-25 06:45:10 +0100

    adding staticfiles configuration

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# this is our root url file
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
