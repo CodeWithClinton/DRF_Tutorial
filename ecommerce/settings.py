5c3e8f4cf4d11a9e0e4c146765d8b425fc0e63cb
Clinton Nwachukwu <Clintonmatics@gmail.com>
2023-06-25 06:41:13 +0100

    adding staticfiles configuration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
